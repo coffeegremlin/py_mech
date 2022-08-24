21846cdbb7251b29ac5e4aafb3c95c3e6a4db748
Nate <natemausert@gmail.com>
2022-08-23 21:40:20 -0700

    Python is as dumb as a box of rock, but added file structure, made to do list, linked used assets

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ clock = pygame.time.Clock()
 FPS = 60
 # Might change it to 30 for retro feel later
 
+# Say what you will about ES6 js but at least I don't have to tell it that the color red is red.
+# Defining colors vvv (this is stupid)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+# To do:
+    # Define the game, mech, animation steps, font.
+    # Load music and sounds
+    # Load images, backgrounds, and spritesheets
+    # Write some kind of function to psuedo animate drawing text
+    # Write function to draw background and the GUI eg: the health bar and attacks
+    #  Make two instances of mechs
+    # Write a loop for the game to run
+    # Add logic to quit the game (Don't trap your user in the game)
---
+++ mech.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Mech():
+    def __init__(self, player, x, y, flip, data)
+    # Need to add a sprite sheet, sound, and animation logic
+    self.player = player
+    self.size = data[0]
+    self.offset = data[2]
+
+# To do:
+    # Load images, movement, maybe gravity logic (idk how fucking stupid python is inherently), animation update renders.
+    # Add an attack and maybe a special attack logic
