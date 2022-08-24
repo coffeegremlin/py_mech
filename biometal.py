151006ad01e7f825cfb2e98be4eaec6db01d443a
Nate <natemausert@gmail.com>
2022-08-24 14:45:35 -0700

    add resources, sprite sheets, refactor project game and naming schema to a megaman zx fighting game

--- biometal.py
+++ biometal.py
@@ -1,7 +1,7 @@
 import pygame
 
-class Mech():
-    def __init__(self, player, x, y, flip, data)
+class Biometal():
+    def __init__(self, player, x, y, flip, data, sprite_sheet, animation_steps, sound):
     # Need to add a sprite sheet, sound, and animation logic
     self.player = player
     self.size = data[0]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame import mixer
-from mech import mech
+from biometal import Biometal
 
 mixer.init()
 pygame.init()
@@ -32,3 +32,37 @@ WHITE = (255, 255, 255)
     #  Make two instances of mechs
     # Write a loop for the game to run
     # Add logic to quit the game (Don't trap your user in the game)
+
+# The game variables
+start_count = 5
+final_count = pygame.time.get_ticks()
+round_complete = False
+ROUND_COMPLETE_COOLDOWN_PERIOD = 3000
+# Score of player1 and player2
+fight_score = [0, 0]
+
+# The Biometal variables
+BIOMETAL_HX_SIZE = 160
+BIOMETAL_HX_SCALE = 4
+BIOMETAL_HX_OFFSET = [72, 56]
+BIOMETAL_HX_DATA = [BIOMETAL_SCALE, BIOMETAL_SIZE, BIOMETAL_OFFSET]
+
+# Biometal Fx Variables
+BIOMETAL_FX _SIZE = 160
+BIOMETAL_FX _SCALE = 4
+BIOMETAL_FX _OFFSET = [113, 106]
+BIOMETAL_FX _DATA = [BIOMETAL_FX_SCALE, BIOMETAL_FX_SIZE, BIOMETAL_FX_OFFSET]
+
+# Set a background, might add a randomizer?
+# Might set them to an array and use .random to pick one
+# Also not sure how convertalpha is working, need to research
+back_image = pygame.image.load("assets/images/backgrounds/humanbase5.png").convert_alpha()
+
+biometal_hx_sheet = pygame.image.load("assets/images/Model-HX/")
+biometal_fx_sheet = pygame.image.load("assets/images/Model-FX/")
+
+# Need to add a victory effect maybe text or an image here
+
+# Amount of steps in each animation frame from sheet
+FX_ANIMATION_STEPS = []
+HX_ANIMATION_STEPS = []
